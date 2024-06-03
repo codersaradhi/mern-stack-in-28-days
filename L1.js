a518f3e5a18fc563cacf8f64ed6e1ae24fd6f427
codersaradhi <chaitanyasaradhi1999@gmail.com>
2024-06-03 13:04:45 +0530

    complete untill for

---
+++ L1.js
@@ -0,0 +1,49 @@
+////////////////////////////////////////Variables
+// const can't redeclared the value
+// const name = 'chaitanya';
+// name = 'saradhi';
+// console.log(name);
+// let can't redeclare the value
+// let country = 'india';
+// country = 'bharat';
+// console.log(country);
+//var is global variable and also it can redclared
+// var state = 'AP';
+// var state = 'godavari';
+// console.log(state);
+///////////////////////////////////mathmatical and relational operations////////////////////////////////
+// const a = 10;
+// const b = 20;
+// console.log('Addition:', a+b)
+// console.log('subraction:', a-b)
+// console.log('multiply:', a*b)
+// console.log('divison:', a%b)
+// if(a>b){
+//     console.log("a value is greater than b")
+// }
+// else{
+//     console.log('b value is larger than a');
+// }
+////////////////////////////////////////////////logical operators and ternary opertor///////////////////
+// const a = 20;
+// const b = 10;
+///////////////// logical op
+// if((a>b) && (a-b == 20)){
+//     console.log('a is greater that b');
+// }
+// else{
+//     console.log('b is greater than a');
+// }
+/////////////////////// ternary operator
+// (((a>b) && (a-b == 20))?console.log('a is greater than b'):console.log('b is greater than a'));
+////////////////////////////////////////////////////for loops //
+// let size =5;
+// for(let a =0;a<size;a++){
+// let starRow ='';
+//     // console.log("*");
+//     for(let b=0;b<size;b++){
+//         starRow += '*';
+//     }
+//     console.log(starRow);
+//     // console.log('\n');
+// }
